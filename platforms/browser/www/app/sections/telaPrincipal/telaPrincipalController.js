a2047e698a4f621e048aeeb5e10b24caf8badbaf
dev <dev>
2016-04-23 21:30:18 -0300

    upgrade cabecalho

--- platforms/browser/www/app/sections/telaPrincipal/telaPrincipalController.js
+++ platforms/browser/www/app/sections/telaPrincipal/telaPrincipalController.js
@@ -1,5 +1,15 @@
 'use strict';
 
-app.controller('telaPrincipalController', ['$scope', '$stateParams', function($scope, $stateParams){
-  $scope.message = $stateParams.urlId;
+app.controller('telaPrincipalController', ['$scope', '$stateParams', '$http', function($scope, $stateParams, $http){
+  $http({
+    method: 'GET',
+    url: 'http://dev.4all.com:3003/tarefa/'+$stateParams.urlId,
+
+  }).then(function successCallback(response) {
+    $scope.responseJson = response.data;
+    console.log(response);
+  }, function errorCallback(response) {
+    console.log(response);
+  });
+
 }]);
