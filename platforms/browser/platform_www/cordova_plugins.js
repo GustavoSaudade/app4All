76e0d3e69990089d989f2952bc94aee12dd5fc25
dev <dev>
2016-04-24 20:06:04 -0300

    upgrade maps

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,9 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
+        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
+        "pluginId": "plugin.google.maps",
+        "clobbers": [
+            "plugin.google.maps"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-whitelist": "1.2.2",
+    "plugin.google.maps": "1.3.9",
+    "com.google.playservices": "21.0.0"
 }
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/app/sections/telaPrincipal/telaPrincipalController.js
+++ platforms/browser/www/app/sections/telaPrincipal/telaPrincipalController.js
@@ -7,9 +7,22 @@ app.controller('telaPrincipalController', ['$scope', '$stateParams', '$http', fu
 
   }).then(function successCallback(response) {
     $scope.responseJson = response.data;
+    $scope.imagemDaPaginaUrl = response.data.urlFoto;
+    $scope.titulo = response.data.titulo;
+    $scope.logoUrl = response.data.urlLogo;
+    $scope.textoDaSecao = response.data.texto;
+    $scope.telefone = response.data.telefone;
     console.log(response);
   }, function errorCallback(response) {
     console.log(response);
   });
+
+
+
+  $scope.clickSearch = function() {
+    alert("SearchAction!");
+  }
+
+  
 
 }]);
