749f6fc24233ef07844cc7ee9b68d1ce3feb2570
Gustavo Kluwe Saudade <gustavo.saudade@cwinet.local>
2016-04-25 14:40:41 -0300

    upgrade

--- platforms/browser/www/app/sections/telaPrincipal/telaPrincipalController.js
+++ platforms/browser/www/app/sections/telaPrincipal/telaPrincipalController.js
@@ -12,17 +12,44 @@ app.controller('telaPrincipalController', ['$scope', '$stateParams', '$http', fu
     $scope.logoUrl = response.data.urlLogo;
     $scope.textoDaSecao = response.data.texto;
     $scope.telefone = response.data.telefone;
+    $scope.latitude = response.data.latitude;
+    $scope.longitude = response.data.longitude;
+    $scope.endereco = response.data.endereco;
+    initialize();
     console.log(response);
   }, function errorCallback(response) {
     console.log(response);
   });
 
-
+  $scope.showPopUp = false;
+  //$scope.$on('$viewContentLoaded', initialize());
 
   $scope.clickSearch = function() {
     alert("SearchAction!");
   }
 
-  
+  $scope.clickEndereco =function() {
+    $scope.showPopUp = true;
+  }
+
+  $scope.clickClosePopUp = function() {
+    $scope.showPopUp = false;
+  }
+
+  function initialize() {
+    var myLatLng = {lat: $scope.latitude, lng: $scope.longitude};
+
+    var map = new google.maps.Map(document.getElementById('map_canvas'), {
+      zoom: 15,
+      center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!',
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+  }
 
 }]);
